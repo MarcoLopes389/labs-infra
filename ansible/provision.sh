00da77a5673d7975c46d68dc35a9749456aa6e27
Marco Lopes <marco.lopes.araujo@gmail.com>
2022-08-05 21:14:27 -0300

    maked folder structure

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -1,8 +1,7 @@
 # Installing ansible
-
 echo "Installing ansible"
-
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install python3 -y
-sudo apt install python3-pip -y
-python3 -m pip install --upgrade ansible
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update -y && sudo apt-get install ansible -y
--- start.sh
+++ start.sh
@@ -1,17 +1,20 @@
+vagrant plugin install vagrant-vbguest
+sudo bash -c 'cat <<EOF > /etc/vbox/networks.conf
+* 10.0.0.0/8 10.0.0.0/16
+* 2001::/64
+EOF'
 cd sonar
 vagrant up
-vagrant ssh -c "hostname -I | cut -d' ' -f2" > /ansible/hosts
 cd ..
 cd kubernetes
 vagrant up
-vagrant ssh -c "hostname -I | cut -d' ' -f2" > /ansible/hosts
 cd ..
 cd jenkins
 vagrant up
-vagrant ssh -c "hostname -I | cut -d' ' -f2" > /ansible/hosts
 cd ..
 cd ansible
 vagrant up
-vagrant upload /ansible/hosts /ansible/hosts
-vagrant upload /ansible/inventory /ansible/inventory
-vagrant ssh -c "ansible-playbook -i /ansible/inventory /ansible/playbook.yml"
+vagrant upload hosts
+vagrant upload roles
+vagrant upload ansible.cfg
+# vagrant ssh -c "ansible-playbook -i /ansible/inventory /ansible/playbook.yml"
