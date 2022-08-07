449948510c177185a5025c3460a7143a334f27a0
Marco Lopes <marco.lopes.araujo@gmail.com>
2022-08-06 21:29:48 -0300

    added ssh config and other service to playbooks

--- config.sh
+++ config.sh
@@ -1,6 +1,12 @@
 # This needs to be run with sudo
 
+# Install vagrant
+apt install -y vagrant
+
+# Install vbguest for virtualbox
 vagrant plugin install vagrant-vbguest
+
+# Set the ip ranges
 bash -c 'cat <<EOF > /etc/vbox/networks.conf
 * 10.0.0.0/8 10.0.0.0/16
 * 2001::/64
--- start.sh
+++ start.sh
@@ -1,17 +1,35 @@
-cd sonar
-vagrant up
-cd ..
-cd kubernetes
-vagrant up
-cd ..
-cd jenkins
-vagrant up
-cd ..
+# Init the vagrant machines
 cd ansible
 vagrant up
+
+# Upload files to the vagrant machine
 vagrant upload jenkins
 vagrant upload sonar
 vagrant upload kubernetes
 vagrant upload ansible.cfg
 vagrant upload inventory
+
+# Put ssh keys of ansible machine in others machines
+vagrant ssh -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y"
+vagrant ssh -c "cat ~/.ssh/id_rsa.pub" > ../ssh_key.txt
+
+cd ..
+cd sonar
+vagrant up
+vagrant upload ../ssh_key.txt
+vagrant ssh -c "tee -a ~/.ssh/authorized_keys < ssh_key.txt"
+cd ..
+cd kubernetes
+vagrant up
+vagrant upload ../ssh_key.txt
+vagrant ssh -c "tee -a ~/.ssh/authorized_keys < ssh_key.txt"
+cd ..
+cd jenkins
+vagrant up
+vagrant upload ../ssh_key.txt
+vagrant ssh -c "tee -a ~/.ssh/authorized_keys" < ../ssh_key.txt
+
+cd ..
+cd ansible
+# Run ansible playbook
 vagrant ssh -c "ansible-playbook -i inventory jenkins/main.yaml"
